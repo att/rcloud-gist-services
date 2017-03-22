14888f4bbdf5ea384193f42b248bb17d71448274
Andrew Dunn <adunn@mango-solutions.com>
2017-03-22 16:13:24 +0000

    Added on tests for the gist error class.

---
+++ rcloud-gist-service/src/test/java/com/mangosolutions/rcloud/rawgist/repository/GistErrorTest.java
@@ -0,0 +1,31 @@
+package com.mangosolutions.rcloud.rawgist.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mangosolutions.rcloud.rawgist.repository.GistError.GistErrorCode;
+
+
+public class GistErrorTest {
+
+	
+	@Test
+	public void gistErrorFormattingTest() {
+		
+		GistError error = new GistError(GistErrorCode.ERR_COMMENT_NOT_EXIST, "This message should be {}", "happy");
+		Assert.assertEquals("This message should be {}", error.getMessage());
+		Assert.assertEquals("ERR_COMMENT_NOT_EXIST: This message should be happy", error.getFormattedMessage());
+		Assert.assertEquals("ERR_COMMENT_NOT_EXIST: This message should be happy", error.toString());
+		
+	}
+	
+	@Test
+	public void gistErrorFormattingNoParamsTest() {
+		
+		GistError error = new GistError(GistErrorCode.ERR_COMMENT_NOT_EXIST, "This has a token, but should not be replaced {}");
+		Assert.assertEquals("This has a token, but should not be replaced {}", error.getMessage());
+		Assert.assertEquals("ERR_COMMENT_NOT_EXIST: This has a token, but should not be replaced {}", error.getFormattedMessage());
+		Assert.assertEquals("ERR_COMMENT_NOT_EXIST: This has a token, but should not be replaced {}", error.toString());
+		
+	}
+}
